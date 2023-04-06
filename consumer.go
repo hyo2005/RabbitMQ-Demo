8a7cb74e3b9207ab03fbed940d1bc6fa75f3fb12
hyo2005 <85051467+hyo2005@users.noreply.github.com>
2023-04-06 09:44:47 +0700

    Update consumer.go

--- consumer.go
+++ consumer.go
@@ -8,20 +8,21 @@ import (
 
 func main() {
 	fmt.Println("this is Consumer")
+	//tao connection den port 5672 voi id va password
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
 	defer conn.Close()
-
+	//khoi tao channel trong queue
 	ch, err := conn.Channel()
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
 	defer ch.Close()
-
+	// nhan message tu queue	
 	msgs, err := ch.Consume(
 		"Test",
 		"",
@@ -31,7 +32,8 @@ func main() {
 		false,
 		nil,
 	)
-
+	
+	// in message ra console
 	blk := make(chan bool)
 	go func() {
 		for msg := range msgs {
